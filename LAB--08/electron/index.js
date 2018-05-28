bb824198c20de99b30d530b7658523ecd5cafda3
ejemplo <diana.gutierrez@tecsup.edu.pe>
2018-05-28 15:44:30 -0500

    Lab08

---
+++ LAB--08/electron/index.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const glob = require('glob')
+
+
+const {app, BrowserWindow, Menu} = require('electron')
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if(process.platform != 'darwin')
+        app.quit();
+});
+
+
+let template = [{
+    label: 'Menu Tecsup',
+    submenu: [{
+        label: 'Hola DaWa',
+        accelerator: 'CmdOrCtrl+Z',
+        role:'undo'
+    }, {
+        type: 'separator'
+    }, {
+        label: 'View',
+        submenu: [{
+            label:'Reload',
+            accelerator: 'CmdOrCtrl+R',
+            click: (item, focusedWindow) => {
+                if(focusedWindow){
+                    if(focusedWindow.id === 1) {
+                        BrowserWindow.getAllWindows().forEach(win => {
+                            if(win.id > 1) win.close()
+                        })
+                    }
+                    focusedWindow.reload()
+                }
+            }
+        }, {
+            label: 'Toggle Full Screem',
+            accelerator: (() => {
+                if(process.platform === 'darwin'){
+                    return 'Ctrl+Command+F'
+                }else{
+                    return 'F11'
+                }
+            })(),
+            click: (item, focusedWindow) => {
+                if (focusedWindow){
+                focusedWindow.setFullScreen(!focusedWindow.isFullScreem())
+                }
+            }
+        }, {
+            type: 'separator'
+        }, {
+            label: 'Toggle Developer Tools',
+            accelerator: (() => {
+                if(process.platform === 'darwin'){
+                    return 'Alt+Command+I'
+                }else{
+                    return 'Ctrl+Shift+I'
+                }
+                
+            })(),
+            click: (item, focusedWindow) => {
+                if(focusedWindow){
+                    focusedWindow.toggleDevTools()
+                }
+            }
+        }]}
+             ]
+}];
+app.on('ready', function() {
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+    
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height:600});
+    
+mainWindow.loadURL(`file://${__dirname}/index.html`);
+    
+mainWindow.openDevTools();
+    
+mainWindow.open('closed', function(){
+    mainWindow = null;
+});
+});
