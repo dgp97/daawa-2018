6a51fa8686e7c51f2535dec05c10174a79d921a3
ejemplo <diana.gutierrez@tecsup.edu.pe>
2018-06-24 21:19:07 -0500

    version2

---
+++ lab11_v2/src/containers/Blog/Blog.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+
+import classes from './Blog.css';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+
+
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+import axios from '../../axios-blog';
+export default class Blog extends Component {
+	state = {
+		posts: null
+	}
+	componentDidMount(){
+        axios.get('posts?_limit=3')
+        .then(response => this.setState({
+            posts: response.data
+        }));
+        
+    }
+	onPostClickHandler = (postId) => {
+        this.props.history.push('/posts/'+postId);
+    }
+	render(){
+		let content = (<Spinner />);
+		if(this.state.posts!=null){
+			content = this.state.posts.map(item => {
+				return (<Post
+					key={item.id}
+					postId={item.id}
+					title={item.title}
+					userId={item.userId}
+					clicked={this.onPostClickHandler} />);
+			});
+		}
+		return(<div className={classes.Blog}>
+			{content}
+			<br />
+			<hr />
+			<Route path="/posts/:id" component={FullPost} />
+		</div>);
+	}
+}
